b0dbcc9ccdd4cc4de623112246e2c5e3e37eb4c0
Daniela Espinoza <danes@roandai.com>
2023-11-20 08:44:41 -0600

    add changes

---
+++ src/app/a1/a1.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { A1Component } from './a1.component';
+
+describe('A1Component', () => {
+  let component: A1Component;
+  let fixture: ComponentFixture<A1Component>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [A1Component]
+    });
+    fixture = TestBed.createComponent(A1Component);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { A1Component } from './a1/a1.component';
+import { MenuComponent } from './menu/menu.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'a1', component: A1Component },
+  { path: '', redirectTo: '/menu', pathMatch: 'full' },
+  { path: '**', component: MenuComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
